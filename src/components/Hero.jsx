4561af3e6998b973b6a1c58b6c6ce17e4fc15c22
Daryl Thomas <dthomas@glimsol.com>
2022-09-12 03:08:47 +0800

    Added Hero page style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,14 @@ import programmingGif from '../assets/programming.gif'
 export default function Hero() {
   
   return (
-    <div className='row'>
-      <section className='col'>
-        <div>Hi! there,</div>
-        <div>I'm Daryl Thomas</div>
-        <div>I'm a Full Stacked Developer</div>
+    <div id='hero' className='container'>
+      <section id='hero-text' className='row'>
+        <div className=''>Hi! there,</div>
+        <div>I'm <span>Daryl Thomas</span></div>
+        <div>I'm a <span>Full Stacked Developer</span></div>
       </section>
-      <section className='col'>
-        <div>
+      <section id='hero-gif' className='row justify-content-end h-100'>
+        <div className='col-5'>
           <img src={programmingGif} alt='programming'/>
         </div>
       </section>
